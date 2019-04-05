50be862b3ab78e37ecd263f1b1d7c6b7f12771a2
saltchang <kk3684635@gmail.com>
2019-04-05 13:20:17 +0800

    changed env var

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
         TESTING = False
         SECRET_KEY = os.environ.get("SECRET_KEY")
         SQLALCHEMY_TRACK_MODIFICATIONS = False
-        SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
+        SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
     class Production(Default):
         ENV = "production"
