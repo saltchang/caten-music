6f686b58186ed0a730f08d246828d96cf9813366
saltchang <kk3684635@gmail.com>
2019-05-25 16:39:47 +0800

    rebuild static files struct

---
+++ caten_worship/static/js/pages/surfer.js
@@ -0,0 +1,17 @@
+// static/js/surfer.js
+
+// Collapse 按鈕 icon 變換
+
+$("h5.surferClass").on("click", "a[aria-expanded='true']", toDown);
+function toDown(event) {
+    $(this).children(".fas").removeClass("fa-chevron-up");
+    $(this).children(".fas").addClass("fa-chevron-down");
+    $(this).css("color", "#a5679b");
+}
+
+$("h5.surferClass").on("click", "a[aria-expanded='false']", toUp);
+function toUp(event) {
+    $(this).children(".fas").removeClass("fa-chevron-down");
+    $(this).children(".fas").addClass("fa-chevron-up");
+    $(this).css("color", "#7e0c0c");
+}
--- caten_worship/static/js/songs/resultlist.js
+++ caten_worship/static/js/songs/resultlist.js
@@ -1,7 +1,7 @@
-// static/js/main.js
+// static/js/resultlist.js
 
 // Collapse 按鈕 icon 變換
-// Songs list
+
 $("div.show-more-song-content").on("click", "a[aria-expanded='true']", collapseArrowToDown);
 function collapseArrowToDown(event) {
     $(this).children(".fas").removeClass("fa-chevron-up");
@@ -16,19 +16,4 @@ function collapseArrowToUp(event) {
     $(this).children(".fas").addClass("fa-chevron-up");
     $(this).parent().addClass("show-more-song-color");
     $(this).parent().parent().parent().removeClass("card-body-border-bottom");
-}
-
-// Surfer
-$("h5.surferClass").on("click", "a[aria-expanded='true']", toDown);
-function toDown(event) {
-    $(this).children(".fas").removeClass("fa-chevron-up");
-    $(this).children(".fas").addClass("fa-chevron-down");
-    $(this).css("color", "#a5679b");
-}
-
-$("h5.surferClass").on("click", "a[aria-expanded='false']", toUp);
-function toUp(event) {
-    $(this).children(".fas").removeClass("fa-chevron-down");
-    $(this).children(".fas").addClass("fa-chevron-up");
-    $(this).css("color", "#7e0c0c");
 }
