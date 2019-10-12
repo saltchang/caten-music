52f3028506190003299cb48e63ce5c634d883e38
saltchang <saltchang.tw@gmail.com>
2019-10-12 16:29:19 +0800

    edit Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.7
 COPY . /app
 WORKDIR /app
-RUN pip install -r requirements.txt
+RUN pip3 install -r requirements.txt
 EXPOSE 5000
 CMD python run.py
--- caten_music/models/base.py
+++ caten_music/models/base.py
@@ -7,11 +7,11 @@ from flask_sqlalchemy import SQLAlchemy
 from caten_music import config
 
 app = Flask(__name__)
-print("models.base")
 
 if config == "Development":
     print("Development")
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+    # psycopg2.connect(dbname=os.environ.get("DB_DEV_DBNAME"), user=os.environ.get("DB_DEV_USER"), password=os.environ.get("DB_DEV_PASSWORD"), host=os.environ.get("DB_DEV_HOST"), port=os.environ.get("DB_DEV_PORT"))
 elif config == "Testing":
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 elif config == "Production":
