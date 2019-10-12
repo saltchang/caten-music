7260ea2011a455d05c59ddcfd38fa983394f6f19
saltchang <saltchang.tw@gmail.com>
2019-10-12 17:25:40 +0800

    add dev mode tag at homepage

--- caten_music/routes/home.py
+++ caten_music/routes/home.py
@@ -8,6 +8,7 @@ from flask_login import current_user
 import random
 import requests
 import json
+import os
 
 home_bp = Blueprint("home_bp", __name__,
                     template_folder='templates')
@@ -29,4 +30,6 @@ def seeHome():
         if r.status_code == 200:
             result = json.loads(r.text)
             
-    return render_template("pages/home.html", songs=result), 200
+    
+    env_state = os.environ.get("APP_SETTING")
+    return render_template("pages/home.html", songs=result, env_state=env_state), 200
