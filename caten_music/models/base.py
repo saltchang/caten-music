e37a77e1ee9bbc0e0b1108153526bbe7230db14a
saltchang <saltchang.tw@gmail.com>
2019-10-12 17:06:24 +0800

    fix stupid mistake in models

--- caten_music/models/base.py
+++ caten_music/models/base.py
@@ -7,15 +7,17 @@ from flask_sqlalchemy import SQLAlchemy
 from caten_music import config
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 if config == "Development":
     print("Development")
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL_FOR_DEVELOPMENT")
-    # psycopg2.connect(dbname=os.environ.get("DB_DEV_DBNAME"), user=os.environ.get("DB_DEV_USER"), password=os.environ.get("DB_DEV_PASSWORD"), host=os.environ.get("DB_DEV_HOST"), port=os.environ.get("DB_DEV_PORT"))
+
 elif config == "Testing":
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
-elif config == "Production":
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL_FOR_TESTING")
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+elif config == "Production":
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+    
 
 db = SQLAlchemy(app)
